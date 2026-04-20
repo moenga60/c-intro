ac2f490a7c4e54b5b9bf231b02e555f53a297c40
moenga60 <moengadaniel60@gmail.com>
2026-04-20 20:34:13 +0300

    first c# commit

---
+++ WebApp/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//services
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+//middleware
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
