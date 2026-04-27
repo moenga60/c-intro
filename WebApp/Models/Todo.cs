9b28ea844ab30c756a4e33b003c20d3db85c5c2f
moenga60 <moengadaniel60@gmail.com>
2026-04-27 16:55:02 +0300

    User Auth in C#

--- WebApp/Models/Todo.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApp.Models;
-
-public class Todo
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public bool IsCompleted { get; set; }
-}
